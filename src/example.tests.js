5d88c046f92330de2a0d4d69e083f068381f6104
Matthew Manela <mmanela@gmail.com>
2016-01-24 10:24:27 -0800

    initial

---
+++ src/example.tests.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    describe("Tests", function () {
+
+        it("tests A", function () {
+
+            expect(false).toEqual(true);
+        });
+        it("tests B", function () {
+
+            expect(false).toEqual(true);
+        });
+    });
+})();
